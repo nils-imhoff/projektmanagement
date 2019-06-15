53086453795aaf9ce2fad1f413d544444ddab478
phisinger <philipsinger@mail.de>
2019-06-15 22:59:11 +0200

    Update Fach-Logic Add Answer-Card

--- app/src/main/java/com/example/projektmanagment/DisplayMessageActivity.java
+++ app/src/main/java/com/example/projektmanagment/DisplayMessageActivity.java
@@ -1,6 +1,8 @@
 package com.example.projektmanagment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,14 +16,18 @@ public class DisplayMessageActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_fach);
+        setContentView(R.layout.content_faecher);
 
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        String messageFach = intent.getStringExtra(FachActivity.EXTRA_FACH);
+        String messageGrad = intent.getStringExtra(FachActivity.EXTRA_GRAD);
 
         // Capture the layout's TextView and set the string as its text
-        TextView textView = findViewById(R.id.textView14);
-        textView.setText(message);
+        TextView dynamicTextView = new TextView(this);
+        dynamicTextView.setLayoutParams(new ConstraintLayout.LayoutParams(
+                ConstraintLayout.LayoutParams.WRAP_CONTENT,
+                ConstraintLayout.LayoutParams.WRAP_CONTENT));
+        dynamicTextView.setText(messageFach);
     }
 }
--- app/src/main/java/com/example/projektmanagment/FachActivity.java
+++ app/src/main/java/com/example/projektmanagment/FachActivity.java
@@ -1,6 +1,7 @@
 package com.example.projektmanagment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,21 +11,27 @@ import android.widget.EditText;
 
 public class FachActivity extends AppCompatActivity {
 
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    public static final String EXTRA_FACH = "Hier steht das Fach";
+    public static final String EXTRA_GRAD = "Hier steht der Grad";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_fach);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fach);
         }
 
 
     /** Called when the user taps the Send button */
     public void sendMessage(View view)  {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+        EditText editFach = (EditText) findViewById(R.id.fach);
+        String messageFach = editFach.getText().toString();
+
+        EditText editGrad = (EditText) findViewById(R.id.grad);
+        String messageGrad = editGrad.getText().toString();
+
+        intent.putExtra(EXTRA_FACH, messageFach);
+        intent.putExtra(EXTRA_GRAD, messageGrad);
         startActivity(intent);
     }
 }
