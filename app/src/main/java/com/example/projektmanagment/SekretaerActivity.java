50ae047874aa65a31365f3b3dbbba4cd677b3ea5
Nils Imhoff <nils.imhoff@hpe.com>
2019-06-16 10:15:24 +0200

    add Öffnungszeiten

--- app/src/main/java/com/example/projektmanagment/SekretaerActivity.java
+++ app/src/main/java/com/example/projektmanagment/SekretaerActivity.java
@@ -28,10 +28,13 @@ public class SekretaerActivity extends AppCompatActivity {
         ListView mListView = findViewById(R.id.listView);
         ArrayList<Sekretaer> sekretaers = new ArrayList<>();
         //professoren.add(new Professor("", "", "", "", ""));
-        final Sekretaer s1 = new Sekretaer("Carina Millow", "1.05", "0711/1849-4517", "0711/1849-4510", "carina.millow@dhbw-stuttgart.de");
+        final Sekretaer s1 = new Sekretaer("Carina Millow", "1.05", "0711/1849-4517", "0711/1849-4510", "carina.millow@dhbw-stuttgart.de", "Montag und Freitag: 9.00 bis 10.00 Uhr\n" +
+                "Dienstag bis Donnerstag: 9.00 bis 11.00 Uhr und 12.30 bis 13.30 Uhr");
         sekretaers.add(s1);
-        sekretaers.add(new Sekretaer("Christine Rohne", "1.03", "0711/1849-4513", "0711/1849-4510", "christine.rohne@dhbw-stuttgart.de"));
-        sekretaers.add(new Sekretaer("Andrea Dölker", "1.01", "0711/1849-4516", "0711/1849-4510", "andrea.doelker@dhbw-stuttgart.de"));
+        sekretaers.add(new Sekretaer("Christine Rohne", "1.03", "0711/1849-4513", "0711/1849-4510", "christine.rohne@dhbw-stuttgart.de", "Montag und Freitag: 9.00 bis 10.00 Uhr\n" +
+                "Dienstag bis Donnerstag: 9.00 bis 11.00 Uhr und 12.30 bis 13.30 Uhr"));
+        sekretaers.add(new Sekretaer("Andrea Dölker", "1.01", "0711/1849-4516", "0711/1849-4510", "andrea.doelker@dhbw-stuttgart.de", "Montag und Freitag: 9.00 bis 10.00 Uhr\n" +
+                "Dienstag bis Donnerstag: 9.00 bis 11.00 Uhr und 12.30 bis 13.30 Uhr"));
         SekretaerListAdapter adapter = new SekretaerListAdapter(this, R.layout.adapter_view_layout, sekretaers);
         mListView.setAdapter(adapter);
         mListView.setOnItemClickListener(new android.widget.AdapterView.OnItemClickListener() {
